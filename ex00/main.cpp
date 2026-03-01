b5f3842d8c267ee0df82071d880d61c81bcecc61
khaledhihi <khaled.hihi0@gmail.com>
2026-03-01 17:37:26 +0000

    ex01

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -5,19 +5,20 @@ int main()
 {
 	
 	std::vector<int> numbers;
-	numbers.push_back(3);
-	numbers.push_back(6);
-	numbers.push_back(8);
+	numbers.push_back(1);
 	numbers.push_back(4);
+	numbers.push_back(8);
+	numbers.push_back(9);
 	numbers.push_back(10);
 	numbers.push_back(11);
 
 	if (easyfind(numbers, 10) == 1)
-		std::cout << 10 << " i found it" << std::endl;
+		std::cout << "found :" << 10 << std::endl;
 	else
-		std::cout << 10 << " i did not found it" << std::endl;
+		std::cout <<"I did not found it" << 10 << std::endl;
+		
 	if (easyfind(numbers, 20) == 1)
-		std::cout << 20 << " i found it" << std::endl;
+		std::cout << "found :" << 20 << std::endl;
 	else
-		std::cout << 20 << " i did not found it" << std::endl;
+		std::cout << "I did not found it" << 20 << std::endl;
 }
